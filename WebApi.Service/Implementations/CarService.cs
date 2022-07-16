949f8a430e2d330b3f729a36304d99ab22afd865
Ckala62rus <agr.akyla@mail.ru>
2022-07-16 17:54:12 +0300

    Добавьте файлы проекта.

---
+++ WebApi.Service/Implementations/CarService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.DAL.Interfaces;
+using WebApi.Domain.Entity;
+using WebApi.Service.Interfaces;
+
+namespace WebApi.Service.Implementations
+{
+    public class CarService : ICarService
+    {
+        private readonly ICarRepository _carRepository;
+        public CarService(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public bool Create(Car entity)
+        {
+            _carRepository.Create(entity);
+            return true;
+        }
+
+        public async Task<List<Car>> GetCars()
+        {
+            return await _carRepository.Select();
+        }
+
+        public Car Get(int id)
+        {
+            return _carRepository.Get(id);
+        }
+
+        public Car GetByName(string name)
+        {
+            return _carRepository.GetByName(name);
+        }
+
+        public Car Edit(int id, Car entity)
+        {
+            _carRepository.Update(entity);
+            return entity;
+        }
+
+        public bool Delete(Car entity)
+        {
+            _carRepository.Delete(entity);
+            return true;
+        }
+    }
+}
---
+++ WebApi.Service/Interfaces/ICarService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Domain.Entity;
+
+namespace WebApi.Service.Interfaces
+{
+    public interface ICarService
+    {
+        Task<List<Car>> GetCars();
+        bool Create(Car entity);
+        Car Get(int id);
+        Car Edit(int id, Car entity);
+        bool Delete(Car entity);
+    }
+}
